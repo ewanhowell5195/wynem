e63470d76845e7173ec856309e267cb15e978f98
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-02-01 15:18:37 +0000

    features page guides

--- src/pages/feature/page.js
+++ src/pages/feature/page.js
@@ -13,51 +13,91 @@ export default class extends Page {
     await this.ready
     const check = await fetchJSON(`features/${args.name}`)
     if (!check) return false
+    await fetchJSON("features")
+    await fetchJSON("commands")
     const $ = this.$
     const feature = window[`features/${args.name}`]
     const title = feature.title ?? args.name.replace(/-/g, " ").toTitleCase()
     jQuery("title").text(`${title} - Wynem`)
+    const linkIcon = $("#link-icon").contents()
     $("#title").text(title)
-    $("#subtitle").text(feature.subtitle)
+    $("#subtitle").text(features.find(e => e.id === args.name).description)
     if (feature.commands) {
       const commandTabs = $("#command-tabs")
       const commandLists = $("#command-lists")
-      for (const [type, commands] of Object.entries(feature.commands)) {
+      for (const [type, related] of Object.entries(feature.commands)) {
         commandTabs.append(
-          E("div").addClass("command-tab").append(E("span").text(type))
+          E("div").addClass("command-tab tab").append(E("span").text(type))
         )
         const commandList = E("div").attr("id", `command-list-${type}`).addClass("command-list").appendTo(commandLists)
-        for (const command of commands) {
+        for (const commandData of related) {
+          let commandPath = commandData
+          if (!Array.isArray(commandPath)) commandPath = commandPath.path
+          let command = commands.categories[type]
+          for (let [i, part] of commandPath.entries()) {
+            if (i === commandPath.length - 1) command = command.commands[part]
+            else command = command.categories[part]
+          }
+          let commandName
+          if (type === "prefix") commandName = `e!${commandPath[commandPath.length - 1]}`
+          else if (type === "slash") commandName = `/${commandPath.join(" ")}`
+          else if (type === "context") commandName = command.name ?? commandPath[commandPath.length - 1].replace(/-/g, " ").toTitleCase()
           commandList.append(
-            E("a", { is: "f-a" }).attr("href", `/commands/${type}/${command.path}`).addClass("button secondary").append(E("span").text(command.name))
+            E("h3").text(commandName),
+            E("p").html(commandData.description ?? (Array.isArray(command.description) ? command.description[0] : command.description)),
+            E("a", { is: "f-a" }).attr("href", `/commands/${type}/${commandPath.join("/")}`).addClass("button secondary").append(
+              linkIcon.clone(true),
+              E("span").text("More info...")
+            )
           )
         }
       }
       $(".command-list").first().addClass("selected")
-      if (Object.keys(feature.commands).length === 1) commandTabs.css("display", "none")
+      if (Object.keys(feature.commands).length === 1) {
+        commandTabs.css("display", "none")
+        $("#sidebar").addClass("tabless")
+      }
       else {
-        $(".command-tab").first().addClass("active")
         $(".command-tab").on("click", e => {
           $(".command-list.selected").removeClass("selected")
           $(".command-tab.active").removeClass("active")
           $(e.currentTarget).addClass("active")
           $(`#command-list-${e.currentTarget.textContent}`).addClass("selected")
-        })
+          $(".command").css("display", "none")
+          $(`.${e.currentTarget.textContent}`).css("display", "")
+        }).first().addClass("active")
       }
     }
     addBlocks($("#description"), feature.description, args.name)
+    $("#guides .heading").text(`How to use ${title}`)
+    const guideTabs = $("#guide-tabs")
+    const guideContent = $("#guide-content")
+    for (const [i, guide] of feature.guides.entries()) {
+      guideTabs.append(
+        E("div").attr("data-guide", i).addClass("guide-tab tab").append(guide.name)
+      )
+      const section = E("div").attr("data-guide", i).addClass("guide")
+      addBlocks(section, guide.content, args.name)
+      guideContent.append(section)
+    }
+    $(".guide").first().addClass("selected")
+    $(".guide-tab").on("click", e => {
+      $(".guide.selected").removeClass("selected")
+      $(".guide-tab.active").removeClass("active")
+      $(`.guide[data-guide="${$(e.currentTarget).addClass("active").attr("data-guide")}"]`).addClass("selected")
+    }).first().addClass("active")
+    $(".slash").css("display", "none")
+    $(".context").css("display", "none")
   }
 }
 
 function addBlocks(element, blocks, feature) {
   const section = E("div").addClass("section")
   for (const block of blocks) {
-    if (block === "spacer") {
-      E("div").appendTo(section)
+    if (typeof block === "string") {
+      E("div").addClass("text").html(block).appendTo(section)
     } else if (block.type === "heading") {
       E("div").addClass("heading").html(block.text).appendTo(section)
-    } else if (block.type === "text") {
-      E("div").addClass("text").html(block.text).appendTo(section)
     } else if (block.type === "tablelist") {
       const table = E("table").addClass("tablelist").appendTo(section)
       for (const row of block.rows) {
